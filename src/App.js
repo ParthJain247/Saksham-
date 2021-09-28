d53c8382d4f4b26cc2d48e1c5d82c7ce87f643ca
Sourabh Bucha <sourabhking14@gmail.com>
2021-09-28 14:03:03 +0530

    update responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Homepage from './Components/Homepage';
 import NavigationBar from './Components/NavigationBar';
+import ContactForm from './Components/campaign';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <Homepage />
+      {/* <Homepage /> */}
+      <ContactForm />
     </div>
   );
 }
