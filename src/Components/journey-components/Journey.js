1b46902665ba0e37087a68343405738139317b55
RIYAJHA <riyajha1294@gmail.com>
2021-10-10 14:59:14 +0530

    journey updated acc to design

--- src/Components/journey-components/Journey.js
+++ src/Components/journey-components/Journey.js
@@ -11,27 +11,25 @@ const Journey = () => {
     <>
       <div className="Journey-page">
         <div className="heading">
-          <h1 className="title">JOURNEY</h1>
+          <h1 className="title">Journey</h1>
         </div>
-        <VerticalTimeline>
+        <VerticalTimeline layout="1-column-left" className="journey-timeline">
           {elements.map((element) => {
             return (
               <VerticalTimelineElement
-                className="journey-timeline"
-                contentStyle={{ background: "#ff6347", color: "#fff" }}
+                className="journey-content-main"
+                contentStyle={{ background: "#000", color: "#fff" }}
                 contentArrowStyle={{
-                  borderRight: "7px solid #ff6347 ",
+                  borderRight: "0px",
                 }}
-                iconStyle={{ background: "yellow", color: "#fff" }}
+                iconStyle={{ background: "#2A0944", color: "#fff" }}
                 key={element.id}
                 date={element.date}
                 dateClassName="date"
               >
                 <div className="journey-content">
                   <div className="text-content">
-                    <img src={element.icon} alt="icon" className="image"></img>
-
-                    <h3 className="vertical-timeline-element-title">
+                    <h3 className="vertical-timeline-element-title title">
                       {element.title}
                     </h3>
                   </div>
