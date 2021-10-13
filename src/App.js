25af5a1cdba3537fd6b519a9799d31099df4b9b3
RIYAJHA <riyajha1294@gmail.com>
2021-10-13 20:15:16 +0530

    individual form updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import CampaignsPage from "./Components/campaign";
 import SchoolForm from "./Components/SchoolForm";
 import ContactUsForm from "./Components/ContactUsForm";
 import { Container } from "react-bootstrap";
-import Form from "./Components/individual-forms/different_ways";
+import Form from "./Components/individual-forms/individual/form";
 import Gallery from "./Components/gallery";
 import Journey from "./Components/journey-components/Journey";
 import MainMessages from "./Components/Messages-components/mainMessages";
